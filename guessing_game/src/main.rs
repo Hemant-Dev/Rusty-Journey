74ca936663b77215bcce9c6faf1eaf5d7b7b51c4
Hemant-Dev <phemant092@gmail.com>
2025-02-13 22:54:15 -0800

    feat: add random number generation to guessing game - also added a .gitignore file

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,14 @@
 use std::io;
+use rand::Rng;
 
 fn main(){
     println!("Guessing Game!");
+    
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The Secret Number is {secret_number}");
+    
     println!("Please enter the guess!");
-
+    
     let mut guess = String::new();
 
     io::stdin()
