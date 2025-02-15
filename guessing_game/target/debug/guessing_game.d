37cb0f223bb48482644ddc6cda0853ea601714d4
Hemant-Dev <phemant092@gmail.com>
2025-02-15 13:13:20 +0530

    proj: added variables project to learn about constants and mutability of rust variables

--- guessing_game/target/debug/guessing_game.d
+++ guessing_game/target/debug/guessing_game.d
@@ -1,1 +1,1 @@
-/home/hemant/projects/Rusty-Journey/guessing_game/target/debug/guessing_game: /home/hemant/projects/Rusty-Journey/guessing_game/src/main.rs
+E:\github\Hemant-Dev\Rusty-Journey\guessing_game\target\debug\guessing_game.exe: E:\github\Hemant-Dev\Rusty-Journey\guessing_game\src\main.rs
