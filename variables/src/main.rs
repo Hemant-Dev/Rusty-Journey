e6931092dee6118b9c42d2176374178c3a917601
Hemant-Dev <phemant092@gmail.com>
2025-02-20 00:43:29 -0800

    feat: update variables project to demonstrate array indexing and user input

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,12 +1,34 @@
-fn main() {
-    let  x = 5;
+use std::io;
 
-    let x = x + 1;
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
-    println!("The value of x is: {x}");
+fn main() {
+    // let  x = 5;
+
+    // let x = x + 1;
+    // {
+    //     let x = x * 2;
+    //     println!("The value of x in the inner scope is: {x}");
+    // }
+    // println!("The value of x is: {x}");
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line.");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+    println!("The value of the element at index {index} is: {element}");
+
+
 }
 
 
