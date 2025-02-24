5713dae616d5e5a32c97f7bac30acc6581208b2d
Hemant-Dev <phemant092@gmail.com>
2025-02-25 00:48:55 +0530

    feat: enhance loop example to demonstrate returning values from loops

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,5 +1,18 @@
-fn main() {
-    loop {
-        println!("again");
-    }
+fn main(){
+    // Basic Loop
+    // loop {
+    //     println!("again!");
+    // }
+
+    // Returning values from loops
+    let mut counter = 0;
+    
+    let result = loop {
+        counter += 1;
+        if counter == 10{
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
 }
