3fcd7248b5fd78d9614960e771271fc5acd686e7
Hemant-Dev <phemant092@gmail.com>
2025-02-25 00:54:10 +0530

    feat: add loop labels example to demonstrate disambiguation in nested loops

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -5,14 +5,34 @@ fn main(){
     // }
 
     // Returning values from loops
-    let mut counter = 0;
+    // let mut counter = 0;
     
-    let result = loop {
-        counter += 1;
-        if counter == 10{
-            break counter * 2;
-        }
-    };
+    // let result = loop {
+    //     counter += 1;
+    //     if counter == 10{
+    //         break counter * 2;
+    //     }
+    // };
 
-    println!("The result is {result}");
+    // println!("The result is {result}");
+
+    // Loop labels to Disambiguate between multiple loops
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining  = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("End count = {count}");
 }
