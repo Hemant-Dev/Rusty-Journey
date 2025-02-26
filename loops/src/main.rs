f86e5b6e2912949b81ba8760535aa71e7a530934
Hemant-Dev <phemant092@gmail.com>
2025-02-26 13:39:14 +0530

    refactor: comment out countdown example and update to demonstrate looping through a collection

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -37,10 +37,18 @@ fn main(){
     // println!("End count = {count}");
 
     // While 
-    let mut number = 3;
-    while number != 0 {
-        println!("{number}");
-        number -= 1;
+    // let mut number = 3;
+    // while number != 0 {
+    //     println!("{number}");
+    //     number -= 1;
+    // }
+    // println!("LIFTOFF!!!");
+
+    // Looping through a Collection with for
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+        index += 1;
     }
-    println!("LIFTOFF!!!");
 }
