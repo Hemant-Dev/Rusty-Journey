fe57ae61b8ee51ab9e65fa4fed5e31bb1d442599
Hemant-Dev <phemant092@gmail.com>
2025-03-03 00:18:09 +0530

    feat: enhance temperature converter to support user choice for conversion direction

--- programs/temp_converter/src/main.rs
+++ programs/temp_converter/src/main.rs
@@ -1,7 +1,20 @@
 use std::io;
 
 fn main() {
-    println!("Welcome to temp converter!!");
+    println!("Welcome to Fahrenheit <--> Celsius Temperature Converter!!");
+    let mut choice = String::new();
+    println!("Enter 1 for Fahrenheit -> Celsius OR 2 for Celsius -> Fahrenheit: ");
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("failed to get the choice from the input!");
+    
+    let choice: u32 = match choice.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Please enter a valid choice!!");
+            return;
+        },
+    };
 
     let mut temperature = String::new();
     println!("Enter temperature value: ");
@@ -9,6 +22,7 @@ fn main() {
         .read_line(&mut temperature)
         .expect("failed to get temperature from the input!"); 
     
+
     let temperature: u32 = match temperature.trim().parse() {
         Ok(num) => num,
         Err(_) => {
@@ -16,13 +30,23 @@ fn main() {
             return;
         },
     };
-    let temperatureInFahrenheit = convertToFahrenheit(temperature);
-    println!("Temperature in Cesius is: {temperature}");
-    println!("Temperature in Fahrenheit is: {temperatureInFahrenheit}");
 
+    if choice == 1 {
+        let temperature_in_fahrenheit = convert_to_fahrenheit(temperature);
+        println!("{}",temperature_in_fahrenheit);
+    } else {
+        let temperature_in_celsius = convert_to_celsius(temperature);
+        println!("{}",temperature_in_celsius);
+    }
 
 }
 
-fn convertToFahrenheit(temperatureInCelsius: u32) -> u32 {
-    return (temperatureInCelsius * 9 / 5) + 32;
+fn convert_to_fahrenheit(temperature_in_celsius: u32) -> u32 {
+    println!("Temperature in Fahrenheit -> ");
+    return (temperature_in_celsius * 9 / 5) + 32;
+}
+
+fn convert_to_celsius(temperature_in_fahrenheit: u32) -> u32 {
+    println!("Temperature in Cesius -> ");
+    return (temperature_in_fahrenheit - 32) * 5 / 9;
 }
